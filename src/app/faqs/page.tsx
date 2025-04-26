8f1e2f3d5e213a682bed316e3e7867e64e5f847e
Akpolo Ogagaoghene Prince <ogazboizakpolo@gmail.com>
2025-04-26 23:46:06 +0100

    assign task

--- src/app/faqs/page.tsx
+++ src/app/faqs/page.tsx
@@ -1,9 +1,10 @@
 import About from '@/components/About/About'
+import Faq from '@/components/Faq/Faq'
 import React from 'react'
 
 function page() {
   return (
-    <About/>
+    <Faq/>
   )
 }
 
--- src/components/Services/services.tsx
+++ src/components/Services/services.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Services() {
   return (
     //start from here
-    <div>for lynda</div>
+    <div>for nonso</div>
   )
 }
 
