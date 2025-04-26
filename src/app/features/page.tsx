0176d024b23698950db6d9beeaf831a8edcf8d42
Akpolo Ogagaoghene Prince <ogazboizakpolo@gmail.com>
2025-04-26 23:30:21 +0100

    assign task

---
+++ src/app/features/page.tsx
@@ -0,0 +1,10 @@
+import { FeaturesSection } from '@/components/features-section'
+import React from 'react'
+
+function page() {
+  return (
+    <FeaturesSection/>
+  )
+}
+
+export default page
---
+++ src/components/Services/services.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function Services() {
+  return (
+    //start from here
+    <div>for lynda</div>
+  )
+}
+
+export default Services
